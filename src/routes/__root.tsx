0365f4a14ed28f5a3c96e929ce9f2896adbe746b
ClearToPay <team@cleartopay.com>
2026-09-14 19:48:02 +0000

    site: add shield+hardhat favicon (favicon.svg + head link)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,6 +23,7 @@ export const Route = createRootRoute({
       { name: "twitter:card", content: "summary" },
     ],
     links: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "canonical", href: "https://cleartopay.ctonew.app/" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
